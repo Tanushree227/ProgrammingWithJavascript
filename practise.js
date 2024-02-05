1cf35191999d42e0f8403bc09462ea4851097ec3
Tanushree227 <tanushreedas813@gmail.com>
2024-02-05 22:56:45 +0530

    datatypes in js

--- practise.js
+++ practise.js
@@ -3,3 +3,22 @@ console.log("Hakuna Matata");
 //Variables in JavaScript
 let name = "Tanushree";
 console.log("Hakuna Matata",name);
+
+//datatypes
+/*
+1. int
+2. float
+3. double
+4. char
+5. string
+6. boolean
+7. null
+*/
+
+var num = 2207;
+var floatD = 22.07;
+var doubleD = 22.0799;
+var charD = 'T';
+var strD = "Tanushree";
+var boolD = true;
+var nullD = null;
